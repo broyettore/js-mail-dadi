c91d3eea20abab7f2c34c04a168c43f388501de7
broyettore <broyettore@gmail.com>
2023-02-24 15:30:32 +0100

    Gioco Dadi done

--- Dadi/js/main.js
+++ Dadi/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const playerHand = Math.floor((Math.random() * 5) + 1);
+const computerHand = Math.floor((Math.random() * 5) + 1);
+
+if(playerHand > computerHand) {
+    alert("Player Wins");
+} else if(computerHand > playerHand) {
+    alert("Computer Wins");
+} else {
+    alert("Draw");
+}
+
+console.log(playerHand);
+console.log(computerHand);
